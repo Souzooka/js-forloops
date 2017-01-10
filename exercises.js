1a6c693f78579a48ddeb576ae86a8df47ff77d6a
Souzooka <bazookaharwell@gmail.com>
2017-01-09 23:41:00 -1000

    Exercise 12

--- exercises.js
+++ exercises.js
@@ -245,11 +245,25 @@ Write a function named `generateArrayOfStrings` which takes a single argument `s
 
 */
 
+var miscStorage = [ [], 'Carrots', 9, 'Beets', {}, {name: "Todd B."}, 'Mush' ];
+
+function generateArrayOfStrings(storage) {
+	var newArray = [];
+	for (i=0;i<storage.length;i++) {
+		if (typeof miscStorage[i] === "string") {
+			newArray.push(storage[i]);
+		}
+	}
+	return newArray;
+}
+
+console.log(generateArrayOfStrings(miscStorage));
+
 /* 13) All Grown Up 
 Write a function that will capitalize the first letter in each word in the phrase below. The function will console.log the message: "I've Lived A Life That's Full. I've Traveled Each and Every Highway. But More, Much More Than This. I Did It My Way."  
 */
 
-var myWay = "i've lived a life that's full, i've traveled each and every highway. but more, much more than this. i did it my way."
+var myWay = "i've lived a life that's full, i've traveled each and every highway. but more, much more than this. i did it my way.";
 
 
 /* 14) Back to School
